914022a1afd395a1cb094962c5a414692a3a25d3
imou-com <Dhrub.Walia@triompheglobal.com>
2026-03-28 14:49:27 +0530

    fix db store error

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ app = FastAPI(title="Ganpati Tour & Travels API")
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # Vite default port
+    allow_origins=["http://localhost:5173","https://ganpatitoursandtravel.onrender.com",],  # Vite default port
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
